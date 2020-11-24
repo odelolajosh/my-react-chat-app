11cf0e32d7c146e15f21468fd10b1f9563621bad
Joshua0707 <oladapoodelola@gmail.com>
2020-11-24 19:10:06 +0100

    Updates

---
+++ precache-manifest.21b99b8c7a4374b1444ab3f9771477e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e629b768c01ffebdf6a4279675824a4b",
+    "url": "/my-react-chat-app/index.html"
+  },
+  {
+    "revision": "3723ec7939212e38a620",
+    "url": "/my-react-chat-app/static/css/main.1a82b8c5.chunk.css"
+  },
+  {
+    "revision": "640087885d38cadaf8b3",
+    "url": "/my-react-chat-app/static/js/2.88e1dd93.chunk.js"
+  },
+  {
+    "revision": "57eec414799b75b36bb910d1db195bb7",
+    "url": "/my-react-chat-app/static/js/2.88e1dd93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3723ec7939212e38a620",
+    "url": "/my-react-chat-app/static/js/main.d5e29900.chunk.js"
+  },
+  {
+    "revision": "ebaf9b6ca691f79d7dc2",
+    "url": "/my-react-chat-app/static/js/runtime-main.fc09bc6b.js"
+  },
+  {
+    "revision": "c916e24fc31466f6e3fa30d56b7788d7",
+    "url": "/my-react-chat-app/static/media/basiercircle.c916e24f.otf"
+  },
+  {
+    "revision": "4b22275f59228fdc60228c1be43bfcd2",
+    "url": "/my-react-chat-app/static/media/choiceSelect.4b22275f.svg"
+  },
+  {
+    "revision": "8a8b917c0093f1626079b8a28c530893",
+    "url": "/my-react-chat-app/static/media/doneforyou.8a8b917c.mp3"
+  },
+  {
+    "revision": "e60f0d19446b03fe1fd93420ad8199c9",
+    "url": "/my-react-chat-app/static/media/logo.e60f0d19.svg"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/my-react-chat-app/static/media/opens.3ed9575d.ttf"
+  },
+  {
+    "revision": "d2c6405793388bc5f1c36f8f5ed37b09",
+    "url": "/my-react-chat-app/static/media/startConversation.d2c64057.svg"
+  }
+]);
